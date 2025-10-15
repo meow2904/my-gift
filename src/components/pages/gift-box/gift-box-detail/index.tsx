c5f467d0a9ab1e91426fde7e9cf9e7ee62bafa25
Nguyen Đinh Huy <75441181+meow2904@users.noreply.github.com>
2025-10-15 17:23:35 +0700

    checking slide image

---
+++ src/components/pages/gift-box/gift-box-detail/index.tsx
@@ -0,0 +1,8 @@
+import ParticleBackground from "@/components/pages/test";
+import BoxDetail from "@/components/ui/box-detail";
+
+export default function GiftBoxDetail() {
+    return (
+        <BoxDetail/>
+    )
+}
